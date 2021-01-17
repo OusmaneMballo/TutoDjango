d0dc78dce56a393ca1293ce224dddea019a804d1
OusmaneMballo <ousmanemballo576@gmail.com>
2021-01-17 15:34:21 +0100

    Gestion des space de nom sur les urls

--- sondages/views.py
+++ sondages/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.http import Http404
+
 from .models import Question
 from django.template import loader
 
@@ -8,18 +9,27 @@ from django.template import loader
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 
-    '''template = loader.get_template('sondages/index.html')
+    '''template = loader.get_template('sondages/questions/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))'''
 
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'sondages/index.html', context)
+    '''context = {'latest_question_list': latest_question_list}
+    return render(request, 'sondages/questions/index.html', context)'''
+    return render(request, 'sondages/questions/index.html', {'latest_question_list': latest_question_list})
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    '''
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    '''
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'sondages/questions/detail.html', {'question': question})
 
 
 def results(request, question_id):
