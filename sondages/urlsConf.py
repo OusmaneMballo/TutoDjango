08161e3c57f478d42a4a6f4b0b713ce2465d4a4a
OusmaneMballo <ousmanemballo576@gmail.com>
2021-01-17 18:55:20 +0100

    affichage du resultat de vote

--- sondages/urlsConf.py
+++ sondages/urlsConf.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'sondages'
 urlpatterns = [
     path('index', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
